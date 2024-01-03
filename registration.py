corrected contrast normalization

registration.py
@@ -15,7 +15,7 @@ moving= ants.image_read(base + 'aligned/TRAP_DL_3_561.tiff')
 #fixed = ants.resample_image(image_fixed, np.asarray(image_fixed.shape)// 10, 1, 0)
 
 mytx = ants.registration(fixed=fixed, moving=moving, type_of_transform='SyNRA', reg_iterations=(40, 20, 10, 0),
-                         aff_shrink_factors=(16, 8, 4, 2, 1))
+                         aff_shrink_factors=(16, 8, 4, 1))
 warpedimage = ants.apply_transforms( fixed=fixed, moving=moving, transformlist=mytx['fwdtransforms'])
 
 #fixed = ants.resample_image(image_fixed, np.asarray(image_fixed.shape), 1, 0)